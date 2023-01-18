19f8de81cdabf26d56698b59a34100b84fd0afdf
Saullo <saullorhamon@gmail.com>
2023-01-17 22:09:47 -0300

    config prisma, seed and add routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
-import { PrismaClient } from "@prisma/client"
-import Fastify from "fastify"
 import cors from '@fastify/cors'
+import Fastify from "fastify"
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/', async ()=>{
-  const habits = await prisma.habit.findMany()
-  return habits
-})
-
+app.register(appRoutes)
 
 app.listen({
-  port:3333
-}).then(()=>{
+  port: 3333
+}).then(() => {
   console.log("Server running...")
 })
